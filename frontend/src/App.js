2b53e7c2186b22f423625e13b6754b41b1df52d4
PLASMA\clark <gomzclark@gmail.com>
2023-09-16 10:54:36 +0800

    deleted unecessary files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react';
-import OperatorButton from './OperatorButton';
-import TextBox from './TextBox';
-import Panel from './Panel';
 
 const App = () => {
   const [number, setNumber] = useState(0);
@@ -9,11 +6,6 @@ const App = () => {
 
   return (
     <div className="App">
-      {text}
-      <TextBox number={number} />
-      <OperatorButton handleNum={()=>setNumber(number+1)} text={"+"}/>
-      <OperatorButton handleNum={()=>setNumber(number-1)} text={"-"}/>
-      <Panel number={number} setText={setText}/>
     </div>
   );
 }
