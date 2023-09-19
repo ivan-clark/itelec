9e14bafe98ddf1ac5d89ed087fb7d1916db18e3c
andrekent123@gmail.com <andrekent123@gmail.com>
2023-09-20 05:21:20 +0800

    Added an image of a home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,10 @@
+import homeImage from '../assets/homeHouse.png';
+
 const Home = () => {
     return (
       <>
         <h1>This is home</h1>
+        <img src={homeImage} alt="This is literally a home." />
       </>
     );
   }
