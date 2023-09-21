ccebc9767641d4f11adade04b22b6f4fdc464515
nellahmarie <nellahtwins2@gmail.com>
2023-09-21 23:05:53 +0800

    added greetings and some little design

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Greeting from './Greeting.jsx';
-
+import './modules/design.css';
 const Home = () => {
     return (
       <>
-        <h1>This is home</h1>
+      <h1>This is home</h1>
+      <div className="n">       
         <Greeting name="Nellah"/>
+      </div>
       </>
     );
   }
