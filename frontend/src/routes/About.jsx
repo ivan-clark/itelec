c10bdfd9621e39690598b8ab9f324d955af0b03c
dingdingwall <johndalepardillo@gmail.com>
2023-09-22 18:49:17 +0800

    added css style, changed the background color, and added paragraph

--- frontend/src/routes/About.jsx
+++ frontend/src/routes/About.jsx
@@ -1,7 +1,17 @@
+import './style.css'
 const About = () => {
     return (
       <>
-        <h1>This is about us</h1>
+        <h1>The Story of The Little Prince </h1>
+   <div className='style'>
+        <p>
+On a tiny planet, there lived a Little Prince who cherished knowledge more than any treasure. With books as his companions, he ventured into the cosmos to learn from the universe itself, growing wiser with each encounter. 
+
+   </p>
+
+
+   </div>
+         
       </>
     );
   }
