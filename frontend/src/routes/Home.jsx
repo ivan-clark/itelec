e8d21525047265654a5ed696cf41d2fec9c3e5d8
Raven <ravenjoven15@gmal.com>
2023-09-22 16:25:03 +0800

    Added a h2 qoute for home.jsx

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -2,6 +2,7 @@ const Home = () => {
     return (
       <>
         <h1>This is home</h1>
+        <h2>"Knowledge will bring you the opportunity to make the differences"</h2>
       </>
     );
   }
