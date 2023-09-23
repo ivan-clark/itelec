df696831072bcb968f3bdb661dd4dcb98b60361e
Aijem <aijemalmacin2018@gmail.com>
2023-09-23 09:12:54 +0800

    updated

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,14 +1,11 @@
 const Home = () => {
     return (
       <>
-        <h1>This is home</h1>
-
-        
+        <h1>This is home</h1>  
     <ul>
       <li><a class="active" href="#home">Home</a></li>
       <li><a href="#news">News</a></li>
       <li><a href="#contact">Contact</a></li>
-      <li><a href="#about">About</a></li>
     </ul>
       </>
     );
