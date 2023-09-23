0aa81756f6211f8afd3510301f9a6d29c24b4646
DESKTOP-F1AO0G9\Alaizah Alvarez <alvarezalaizah@gmail.com>
2023-09-23 11:41:01 +0800

    added header in root file

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -3,6 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 const Root = () => {
     return (
       <>
+      <h1>WELCOME TO OUR WEBSITE!</h1>
             <Link to={`/home`}>Home</Link>
             <Link to={`/about`}>About</Link>
             <Outlet />
