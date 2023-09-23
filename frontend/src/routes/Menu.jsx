6e9acaa1098ba3052c0013e75c4a59124d5c6e83
DESKTOP-TNU1G00\VILFAM <kerviemille@gmail.com>
2023-09-23 20:21:12 +0800

    added paragraph in menu(nasayop kog commit sir na direct sa develop sorry)

--- frontend/src/routes/Menu.jsx
+++ frontend/src/routes/Menu.jsx
@@ -2,13 +2,16 @@ const Menu = () => {
     return (
       <>
         <h1>This is menu</h1>
-  <p> OTAN---------------------PHP 15.00</p>
-  <p> MONGGOS---------------------PHP 15.00</p>
-  <p> PALIYA---------------------PHP 15.00</p>
-  <p> HOTDOG---------------------PHP 10.00</p>
-  <p> ITLOG SA DINOSAUR---------------------PHP 150.00</p>
-  <p> SISIG---------------------PHP 200.00</p>
-
+      <center> 
+      <h2>
+         OTAN---------------------PHP 15.00
+        <br/> MONGGOS---------------------PHP 15.00
+        <br/> PALIYA---------------------PHP 15.00
+        <br/> HOTDOG---------------------PHP 10.00
+        <br/> ITLOG SA DINOSAUR---------------------PHP 150.00
+        <br/> SISIG---------------------PHP 200.00
+      </h2>
+      </center>
   
       </>
     );
