7df15a72b0af9f785391c5b7c4b88d89b2e39463
Aijem <aijemalmacin2018@gmail.com>
2023-09-23 08:30:32 +0800

    Moved to the src folder

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -2,6 +2,14 @@ const Home = () => {
     return (
       <>
         <h1>This is home</h1>
+
+        
+    <ul>
+      <li><a class="active" href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
       </>
     );
   }
