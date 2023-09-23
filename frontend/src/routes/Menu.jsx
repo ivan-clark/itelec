4ea7b2e2b8b73939ab12583f6f8d4026b0ba4f55
DESKTOP-TNU1G00\VILFAM <kerviemille@gmail.com>
2023-09-23 11:31:59 +0800

    added some paragraph in the menu

--- frontend/src/routes/Menu.jsx
+++ frontend/src/routes/Menu.jsx
@@ -2,6 +2,14 @@ const Menu = () => {
     return (
       <>
         <h1>This is menu</h1>
+  <p> OTAN---------------------PHP 15.00</p>
+  <p> MONGGOS---------------------PHP 15.00</p>
+  <p> PALIYA---------------------PHP 15.00</p>
+  <p> HOTDOG---------------------PHP 10.00</p>
+  <p> ITLOG SA DINOSAUR---------------------PHP 150.00</p>
+  <p> SISIG---------------------PHP 200.00</p>
+
+  
       </>
     );
   }
