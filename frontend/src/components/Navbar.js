8b4dd67d95ab1aca6d5574dadf02f1a89be63567
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-09-24 14:37:13 +0800

    navbar navbar for page partial

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,41 @@
+/**
+ * Link routes are temporary
+ * can be change
+ */
+import { Link } from "react-router-dom";
+//import "./Navbar.css"; //change import name if necessary
+
+function Navbar() {
+  //add state effect or function scaling
+  return (
+    <>
+      <nav>
+        <div>
+          <Link to="/home">
+            {/**CHANGE to decided name
+             * can add icon after name
+             */}
+            SHOP NAME
+          </Link>
+          <ul>
+            <li className="nav-item">
+              <Link to="/home">HOME</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links">
+                ABOUT US
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-links">
+                CONTACT US
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
