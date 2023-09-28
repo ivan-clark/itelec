8fdfb813ad1cc4f194a8dd1fa83adc2b8288cbc1
adrean paul sorono <adreanpaulsorono@gmail.com>
2023-09-28 12:48:28 +0800

    added initial css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,23 +3,21 @@
  * can be change
  */
 import { Link } from "react-router-dom";
-//import "./Navbar.css"; //change import name if necessary
+import "./Navbar.css";
 
 function Navbar() {
-  //add state effect or function scaling
   return (
     <>
-      <nav>
-        <div>
-          <Link to="/">
-            {/**CHANGE to decided name
-             * can add icon after name
-             */}
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
             SHOP NAME
           </Link>
-          <ul>
+          <ul className="nav-menu">
             <li className="nav-item">
-              <Link to="/home">HOME</Link>
+              <Link to="/home" className="nav-links">
+                HOME
+              </Link>
             </li>
             <li className="nav-item">
               <Link to="/about" className="nav-links">
