3b9a716c58fbee9d6485db01329ac0ca35854d62
nellahmarie <nellahtwins2@gmail.com>
2023-10-06 22:33:23 +0800

    made a contact information in footer

--- frontend/src/routes/ContactUs/Footer.js
+++ frontend/src/routes/ContactUs/Footer.js
@@ -1,24 +1,24 @@
 import React from "react";
-import "./ContactUs.css"
-
+import "./ContactUs.css";
 const Footer = () => {
     return (
         <React.Fragment>
-            <footer id="footerbg">
-                
+            <footer className="footerbg">
                 <div className='container'>
                     <div className='row'>
                         <div className='col-sm-3 footer-contact'>
-                            <h5>Contact Us</h5>
-                            <p>(63)91234567810</p>
-                            <p>(63)91234567810</p>
-                            <p>(63)91234567810</p>
-                        </div>
-                        <div className='col-sm-3 footer-navigation'>
-                            <h5>Navigation</h5>
-                            <p><a href="">Home</a></p>
-                            <p><a href="">About</a></p>
-
+                            <h5 className="contactHeader">For more details please Contact Us:</h5>
+                            <ul className="unbullet-list">
+                                <li className="gmail">
+                                    <strong>Email:</strong> example@email.com
+                                </li>
+                                <li className="phone">
+                                    <strong>Phone:</strong> +1 (123) 456-7890
+                                </li>
+                                <li className="address">
+                                    <strong>Address:</strong> 123 Main Street, City, Country
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
