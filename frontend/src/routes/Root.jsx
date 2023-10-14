3f9d76a39ecda5c74aebf71ad44cc69012468a1f
Michelle Placencia <placenciamitel14@gmail.com>
2023-10-14 09:24:06 +0800

    ????

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 
 const Root = () => {
     return (
       <>
+            <Link to={`/home`}>Home</Link>
+            <Link to={`/about`}>About</Link>
+            <Outlet />
         <Navbar
           home = '/'
           page1='home/'
