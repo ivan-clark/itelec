06df9d45d5f9f6051559e8bcb4282fd7ac6a088c
Hannah <hannahjaneferrer2@gmail.com>
2023-10-14 09:44:24 +0800

    Utilize Header and Footer

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,11 +1,10 @@
-import Footer from '../components/Footer/Footer.jsx';
 
 const Home = () => {
     return (
       <>
         <h1>This is home</h1>
 
-        <Footer />
+   
       </>
       
     );
